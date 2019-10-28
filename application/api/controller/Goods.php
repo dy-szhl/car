75631c7ec709f3344551fdd1a7d5dd68bfa165b0
94Pisces <53338283+94Pisces@users.noreply.github.com>
2019-10-28 18:23:33 +0800

    goods list
    商品列表接口

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -18,4 +18,33 @@ class Goods extends Common
         $data=['list'=>$list,'total_page'=>$info->lastPage()];
         return $this->_resData(1,'获取成功',$data);
     }
+    //商品列表
+    public function goods_list(){
+        $list = [];
+        $where[] = ['status','=',1];
+        $order_sql = 'update_time desc';
+        if(input()){
+            $fields = input('fields');
+            $order = input('order');
+            $keyword = input('keyword');
+            if($fields && $order) $order_sql = $fields.' '.$order;
+            if($keyword) $where[] = ['name','like','%'.$keyword.'%'];
+        }
+        $php_input['order'] = $order_sql;
+        $php_input['where'] = $where;
+        
+        $info = \app\common\model\Goods::getFormatData($php_input)->each(function($item,$index)use(&$list){
+            array_push($list,[
+                'id' => $item['id'],
+                'name' => $item['name'],
+                'img' => $item['img'],
+                'date_time' => $item['create_time'],
+                'price' => $this->user_id?$item['price0']:'登录后查看',
+                'sold_num' => $item['sold_num'],
+            ]);
+        });
+        $data=['list'=>$list,'total_page'=>$info->lastPage()];
+        return $this->_resData(1,'获取成功',$data);
+    }
+
 }
