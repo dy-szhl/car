d334487e73bdec82084566deec3bdf3d0d06dafc
94Pisces <53338283+94Pisces@users.noreply.github.com>
2019-10-28 18:50:43 +0800

    Update Goods.php

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -19,7 +19,8 @@ class Goods extends Common
         return $this->_resData(1,'获取成功',$data);
     }
     //商品列表
-    public function goods_list(){
+    public function goods_list()
+    {
         $list = [];
         $where[] = ['status','=',1];
         $order_sql = 'update_time desc';
@@ -32,7 +33,7 @@ class Goods extends Common
         }
         $php_input['order'] = $order_sql;
         $php_input['where'] = $where;
-        
+
         $info = \app\common\model\Goods::getFormatData($php_input)->each(function($item,$index)use(&$list){
             array_push($list,[
                 'id' => $item['id'],
@@ -44,6 +45,28 @@ class Goods extends Common
             ]);
         });
         $data=['list'=>$list,'total_page'=>$info->lastPage()];
+        return $this->_resData(1,'获取成功',$data);
+    }
+    //商品详情
+    public function goods_detail()
+    {
+        $id = input('id',0,'intval');
+        $model = \app\common\model\Goods::get($id);
+        if(empty($model)){
+            return $this->_resData(0,'商品不存在');
+        }
+        $data = [];
+        $data['goods'] = [
+            'id' => $model['id'],
+            'name' => $model['name'],
+            'img' => $model['img'],
+            'stock' => $model['stock'],
+            'spu' => $model['spu'],
+            'price0' => $model['price0'],
+            'price1' => $model['price1'],
+            'price2' => $model['price2'],
+            'content' => $model['content'],
+        ];
         return $this->_resData(1,'获取成功',$data);
     }
 
